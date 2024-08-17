3addffcf1f33222140cb19f4ae83570568ecf9d7
samira <samira.mouhammed@gmail.com>
2024-08-17 22:12:55 +0100

    changes

--- src/count.js
+++ src/count.js
@@ -1,17 +1,15 @@
 import './App.css';
 import React from 'react';
 
-
-
 export default function Count() {
     const [count, setCount] = React.useState(0);
-    function increace() { setCount(count + 1); }
-    function decreace() { setCount(count - 1); }
+    function increase() { setCount(count + 1); }
+    function decrease() { setCount(count - 1); }
     return (
       <div>
         <h1>{count}</h1>
-        <button onClick={decreace} id="dec">decreace</button>
-        <button onClick={increace} id="inc">increace</button>
+        <button onClick={decrease} id="dec">decrease</button>
+        <button onClick={increase} id="inc">increase</button>
       </div>
       
     );
